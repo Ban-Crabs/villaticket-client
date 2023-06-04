8cceca8472d14b57255083ab7563858abba25424
Diego Engelhard <00356119@uca.edu.sv>
2023-06-04 02:59:11 -0600

    Add: Top section of landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import classes from "./App.module.scss";
+import style from "./App.module.scss";
+import imgBbPlayer from "./assets/image-bb-player.png";
 
 function App() {
 
   return (
     <>
-      {/* PAGE WRAPPER */}
-      <div className={classes["App"]}>
-        
+      <div className={style["top-section"]}>
+        <img className={style["image-bb-player"]} src={imgBbPlayer} alt="img-bb-player" />
+        <h1 className={style["landing-text-title"]}>
+          <span className={style["title"]}>
+            Score MLB <br /> Playoff Tickets! <br />
+          </span>
+          <span className={style["span1"]}> Buy Tickets, Earn Rewards! </span>
+        </h1>
       </div>
     </>
   )
