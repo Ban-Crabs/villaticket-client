b4bcb1cc8f0b36438cbb74ad27621f16d1adf12d
Diego Engelhard <00356119@uca.edu.sv>
2023-06-05 14:25:04 -0600

    Add: QR Page project structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import style from "./App.module.scss";
 
 import HomePage from "./components/Home/HomePage";
 
+import QR from "./components/QrPage/QR";
+
 function App() {
 
   return (
-    <HomePage />
+    <QR />
   )
 }
 
---
+++ src/components/QrPage/QR.jsx
@@ -0,0 +1,11 @@
+import style from "./QR.module.scss";
+
+
+const QR = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default QR;
