0e9c9f05a58acd0205474ef0f6e649e11abf3232
Diego Engelhard <00356119@uca.edu.sv>
2023-06-05 15:00:30 -0600

    Add: Pagination

--- src/components/QrPage/QR.jsx
+++ src/components/QrPage/QR.jsx
@@ -4,6 +4,28 @@ import style from "./QR.module.scss";
 const QR = () => {
     return (
         <>
+            <div className={style["container"]}>
+                <div className={style["title"]}>
+                    <h1>Your QRs</h1>
+                </div>
+                <div className={style["divider"]}></div>
+                <div className={style["qr-list-container"]}>
+                    <div className={style["qr-card"]}>
+                        {/* TODO: IMPLEMENT QR-CARD */}
+                        <span>QR</span>
+                        <span>QR</span>
+                    </div>
+                </div>
+                <div className={style["pagination"]}>
+                    <ul>
+                        <li> <a href="#" className={style["prev"]}>Prev</a> </li>
+                        <li className={style["pageNumber"]}> 1 <a href="#"></a> </li>
+                        <li className={style["pageNumber"]}> 2 <a href="#"></a> </li>
+                        <li className={style["pageNumber"]}> 3 <a href="#"></a> </li>
+                        <li> <a href="#" className={style["next"]}>Next</a> </li>
+                    </ul>
+                </div>
+            </div>
         </>
     )
 }
