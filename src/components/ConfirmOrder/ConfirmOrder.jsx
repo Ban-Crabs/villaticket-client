57d24523e7be154224dc24ea0346175db318c59a
Diego Engelhard <00356119@uca.edu.sv>
2023-06-06 15:49:31 -0600

    Add: Confirm order page template

---
+++ src/components/ConfirmOrder/ConfirmOrder.jsx
@@ -0,0 +1,12 @@
+import style from "./ConfirmOrder.module.scss";
+
+
+const ConfirmOrder = () => {
+    return (
+        <>
+            <h1>Confirm Order</h1>
+        </>
+    )
+}
+
+export default ConfirmOrder;
