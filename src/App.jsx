564d406adb65d148ea2d55f97a65886eaa8ece0b
MarioMartinez00072520 <00072520@uca.edu.sv>
2023-06-05 21:14:00 -0600

    Add: Routing Configuration, Context, AXIOS, and toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import style from "./App.module.scss";
 
+import {Routes, Route} from "react-router-dom";
+
 import HomePage from "./components/Home/HomePage";
 
 import QR from "./components/SearchResults/SearchResults";
 
 function App() {
-
   return (
     <QR />
   )
