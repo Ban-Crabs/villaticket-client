f875c397b296a0cf5a0795d9090ff4b8332e4b1b
Diego Engelhard <00356119@uca.edu.sv>
2023-06-06 21:55:23 -0600

    Add: basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import {Routes, Route} from "react-router-dom";
 
 import HomePage from "./components/Home/HomePage";
 
-import View from "./components/EventDetails/EventDetails";
+import View from "./components/BuyTicket/BuyTicket";
 
 function App() {
   return (
---
+++ src/components/BuyTicket/BuyTicket.jsx
@@ -0,0 +1,12 @@
+import style from "./BuyTicket.module.scss";
+
+
+const BuyTicket = () => {
+    return (
+        <>
+            <h1>BUY TICEKT</h1>
+        </>
+    )
+}
+
+export default BuyTicket;
