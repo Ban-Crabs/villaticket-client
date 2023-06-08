8dc9fff0f6182793408ad34d1df1b7d8959f4cbf
Diego Engelhard <00356119@uca.edu.sv>
2023-06-08 16:57:29 -0600

    Fix: View shrink

--- src/components/ModeratorUserList/ModeratorUserList.jsx
+++ src/components/ModeratorUserList/ModeratorUserList.jsx
@@ -4,45 +4,41 @@ import style from "./ModeratorUserList.module.scss";
 const ModeratorUserList = () => {
     return (
         <>
-            <div className={style["heading"]}>
-                <h5>Showing now:</h5>
-                <h1>All Users</h1>
-            </div>
             <div className={style["container"]}>
 
-                <div className={style["divider"]}></div>
+                <div className={style["heading"]}>
+                    <h5>Showing now:</h5>
+                    <h1>All Users</h1>
 
-                <div className={style["card-description-title"]}>
-                    <ul>
-                        <li><b>Email</b></li>
-                        <li><b>Username</b></li>
-                        <li><b>Permissions</b></li>
+                    <div className={style["divider"]}></div>
+                </div>
+
+                <div className={style["titles"]}>
+                    <ul className={style["list"]}>
+                        <li>Email</li>
+                        <li>Username</li>
+                        <li>Permissions</li>
                     </ul>
                 </div>
 
-                <div className={style["card-list-container"]}>
-
+                <div className={style["cards-container"]}>
                     <div className={style["card-item"]}>
                         <div className={style["card-item-info"]}>
-
-                            <div className={style["card-item-users-info"]}>
-                                <ul>
-                                    <li><b>user@email.com</b></li>
-                                    <li>User123</li>
-                                    <li>Cliente</li>
-                                </ul>
-                            </div>
-                            
+                            <ul className={style["list"]}>
+                                <li>user@email.com</li>
+                                <li>Username123</li>
+                                <li>Client</li>
+                            </ul>
                         </div>
 
-                        <div className={style["button-container"]}>
+                        <div className={style["card-item-btn"]}>
                             <button type="submit">Ban</button>
                         </div>
-
                     </div>
-
                 </div>
+
             </div>
+
         </>
     )
 }
