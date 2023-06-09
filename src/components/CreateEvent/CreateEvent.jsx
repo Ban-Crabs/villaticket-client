2ceb267c72a63b21ef77347272f8c071920b7199
Diego Engelhard <00356119@uca.edu.sv>
2023-06-09 09:16:42 -0600

    Add: Basic page structure

---
+++ src/components/CreateEvent/CreateEvent.jsx
@@ -0,0 +1,19 @@
+// CreateEvent
+import style from "./CreateEvent.module.scss";
+
+
+const CreateEvent = () => {
+    return (
+        <> 
+            <div className={style["container"]}>
+                {/* LEFT SECTION */}
+                <div className={style["left-section"]}></div>
+
+                {/* RIGHT SECTION */}
+                <div className={style["right-section"]}></div>
+            </div>
+        </>
+    )
+}
+
+export default CreateEvent;
