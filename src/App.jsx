964357f48ae2650fdee38a67d7202e0ce7078d6c
Diego Engelhard <00356119@uca.edu.sv>
2023-06-18 16:41:55 -0600

    Fix: User Transfer Ticket view

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import BuyTicket from "./components/BuyTicket/BuyTicket";
 import ConfirmOrder from "./components/ConfirmOrder/ConfirmOrder";
 import OrderSuccessful from "./components/OrderSuccessful/OrderSuccessful";
 
-import TempView from "./components/UserTicketHistory/UserTicketHistory"
+import TempView from "./components/UserTransferTicket/UserTransferTicket"
 
 
 function App() {
