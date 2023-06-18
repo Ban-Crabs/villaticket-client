94aa3e086036c23b1ffa8bbd611b9b47c5bebc30
Diego Engelhard <00356119@uca.edu.sv>
2023-06-17 23:12:17 -0600

    Fix: Footer

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,8 @@ function App() {
     <>
       <Header/>
       <Routes>
-        <Route index element={<HomeView />} />
+        {/* <Route index element={<HomeView />} /> */}
+        <Route index element={<AccountView />} />
         <Route path="/profile/*" element={<AccountView/>}/>
         <Route path="/event" element={<EventDetails/>}/>
         <Route path="/order" element={<BuyTicket/>}/>
